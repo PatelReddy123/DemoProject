d1bf14176f0a22e4b6383e1982e8f4399aac5329
PatelReddy123 <patelreddy97@gmail.com>
2021-10-06 19:32:46 +0530

    B code

--- src/org/git/sample/SourceCode.java
+++ src/org/git/sample/SourceCode.java
@@ -7,15 +7,24 @@ public class SourceCode {
 	private void push() {
 		System.out.println("Push Source Code");
 	}
+
 	private void code() {
 		System.out.println("Source Code");
+	}
+	private void gitSource() {
+	System.out.println("Git Source Code");
+
 
 	}
 	public static void main(String[] args) {
 		SourceCode s = new SourceCode();
 		s.pull();
 		s.push(); 
+
 		s.code();
+
+		s.gitSource();
+
 	}
 
 }
